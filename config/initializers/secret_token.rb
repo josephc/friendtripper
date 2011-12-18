3ca7b96df31afc02f0887ccd0c02d04c99505985
Joseph Childress <joseph@crowdflower.com>
2011-12-18 11:20:58 -0800

    Rails app sucka!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendtripper::Application.config.secret_token = '36ba274381f426b2a106ac86841e16626d6d4728e28f54420396d29dec1ab139e5cf2e19467b16b85baec01fe23ca3762b7d86b5fb88642e4f6f9f8e136efc6a'
