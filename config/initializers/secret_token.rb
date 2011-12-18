43b03caeb7db7898d5d21016fff54d1bbb27fda6
Joseph Childress <joseph@crowdflower.com>
2011-12-18 12:41:25 -0800

    postgres integration part deux

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Friendtripper::Application.config.secret_token = '36ba274381f426b2a106ac86841e16626d6d4728e28f54420396d29dec1ab139e5cf2e19467b16b85baec01fe23ca3762b7d86b5fb88642e4f6f9f8e136efc6a'
+Friendtripper::Application.config.secret_token = 'd53604eb7e745f72e0bc90a251e4f2545c2dc45a2ec2874cfec6736d5734463a79ec221ff2e95e68a03adfb4f34d1bf6f7658b9d5f04c67e40ac6a6b1794c389'
