5909df068b34f124988e81440debef9d269e9b5d
Joseph Childress <joseph@crowdflower.com>
2011-12-21 19:27:08 -0800

    wut

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Friendtripper::Application.config.secret_token = 'd53604eb7e745f72e0bc90a251e4f2545c2dc45a2ec2874cfec6736d5734463a79ec221ff2e95e68a03adfb4f34d1bf6f7658b9d5f04c67e40ac6a6b1794c389'
+Friendtripper::Application.config.secret_token = '34d70d402a9f1dad7c109d969be2b87d2620c24ae9741b536d9e5ebd7753ca973b24cd89bdddc106ad5bb53e5ad30b8226d0720c9134c6db6ed662f3256a6d42'
