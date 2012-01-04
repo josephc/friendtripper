bd9b28256eefb9f31b990cc9b8d3f501ac634ee2
Joseph Childress <joseph@crowdflower.com>
2012-01-03 18:40:12 -0800

    Switch to postgres 2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Friendtripper::Application.config.secret_token = '34d70d402a9f1dad7c109d969be2b87d2620c24ae9741b536d9e5ebd7753ca973b24cd89bdddc106ad5bb53e5ad30b8226d0720c9134c6db6ed662f3256a6d42'
+Friendtripper::Application.config.secret_token = 'be721f4cc616182f530ecd9a0b82814afba96f8ad059ee6bc88eefbaf46252c3b29aa320ca8507685498cc196bc176240a9cc37baf9f6a50e09478ff45ef1ec0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Friendtripper::Application.routes.draw do
-  get "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +48,7 @@ Friendtripper::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
